5115a3f66efaf28da9c569d78245061d1bf7d768
Mr.R <2584110147@qq.com>
2023-02-18 02:37:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 pip install osascript
 sudo cp ancyterm.py /usr/local/bin/ancyterm && sudo chmod +x /usr/local/bin/ancyterm
 sudo cp backend.py /usr/local/bin/backend && sudo chmod +x /usr/local/bin/backend
-sudo cp iTerm2_Backend.py /usr/local/bin/iTerm2_Backend && sudo lchmod +x /usr/local/bin/iTerm2_Backend
+sudo cp iTerm2_Backend.py /usr/local/bin/iTerm2_Backend && sudo chmod +x /usr/local/bin/iTerm2_Backend
