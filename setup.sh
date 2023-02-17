8b9c680fcdbde99ec8a3a21038b1a6309d519a1e
Mr.R <2584110147@qq.com>
2023-02-18 02:56:35 +0800

    set Pwntools context.terminal

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ pip install osascript
 sudo cp ancyterm.py /usr/local/bin/ancyterm && sudo chmod +x /usr/local/bin/ancyterm
 sudo cp backend.py /usr/local/bin/backend && sudo chmod +x /usr/local/bin/backend
 sudo cp iTerm2_Backend.py /usr/local/bin/iTerm2_Backend && sudo chmod +x /usr/local/bin/iTerm2_Backend
+echo '[context]' >> ~/.pwn.conf
+echo 'terminal=["ancyterm","-t","iterm2","-e"]' >> ~/.pwn.conf
