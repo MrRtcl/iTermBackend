dddc3f67fba78865be6e34a190e882b899be9276
Mr.R <2584110147@qq.com>
2023-03-24 23:47:08 +0800

    systemd

--- setup.sh
+++ setup.sh
@@ -4,3 +4,16 @@ sudo cp backend.py /usr/local/bin/backend && sudo chmod +x /usr/local/bin/backen
 sudo cp iTerm2_Backend.py /usr/local/bin/iTerm2_Backend && sudo chmod +x /usr/local/bin/iTerm2_Backend
 echo '[context]' >> ~/.pwn.conf
 echo 'terminal=["ancyterm","-e"]' >> ~/.pwn.conf
+sudo echo "[Unit]
+Description=Backend Service
+After=network.target
+
+[Service]
+Type=simple
+User=`whoami`
+ExecStart=/usr/local/bin/backend
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/backend.service
+sudo systemctl enable backend.service
